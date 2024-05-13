66a2a8436295d7e39c364045e7320a2aac812bb6
S. David Kyle <sdavidkyle@gmail.com>
2024-05-13 16:27:43 -0700

    Examples of Span

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Threading.Tasks.Dataflow;
+
 Console.WriteLine("Hello, World!");
+static int Sum(Span<int> array)
+{
+    int sum = 0;
+    foreach (int i in array)
+    {
+        sum += i;
+    }
+    return sum;
+}
+int[] array = { 1, 2, 3, 4, 5 };
+
+Console.WriteLine(Sum(array));
+Console.WriteLine(Sum(new Span<int>(array)));
+
+
