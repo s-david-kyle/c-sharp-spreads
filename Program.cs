533aed25d230f932320d146cf90ab3d407954de9
S. David Kyle <sdavidkyle@gmail.com>
2024-05-13 18:02:05 -0700

    Implementing Span<T>, very cool

--- Program.cs
+++ Program.cs
@@ -1,17 +1,116 @@
-﻿using System.IO.Pipes;
+﻿using System.Runtime.CompilerServices;
 
-int i = 0;
+int i = 42;
+var span = new MySpan<int>(ref i);
+span[0] = 43;
+Console.WriteLine(i); // 1
 
-// Add the /unsafe flag to enable unsafe code
-// /unsafe
-unsafe
+var anotherSpan = new MySpan<char>("Hello".ToCharArray());
+Console.WriteLine(anotherSpan[0]); // H
+while (anotherSpan.Length > 0)
 {
-    int* ptr = &i;
-    ref int iref = ref *ptr;
+    Console.WriteLine(anotherSpan[0]);
+    anotherSpan = anotherSpan.Slice(1);
 }
 
-
-static void Use(ref int reference, int lentgth)
+readonly ref struct MySpan<T>
 {
+    private readonly ref T _reference;
+    private readonly int _length;
 
+    public int Length => _length;
+
+    public MySpan(T[] array)
+    {
+        _reference = ref array[0];
+        _length = array.Length;
+    }
+
+    public MySpan(ref T reference)
+    {
+        _reference = ref reference;
+        _length = 1;
+    }
+
+    public MySpan(ref T reference, int length)
+    {
+        _reference = ref reference;
+        _length = length;
+    }
+
+    public ref T this[int index]
+    {
+        get
+        {
+            if (index >= _length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            return ref Unsafe.Add(ref _reference, index);
+        }
+
+    }
+
+    public MySpan<T> Slice(int offset)
+    {
+        if (offset > _length)
+        {
+            throw new IndexOutOfRangeException();
+        }
+
+        return new MySpan<T>(ref Unsafe.Add(ref _reference, offset), _length - offset);
+    }
+
+
+}
+
+readonly ref struct MyReadOnlySpan<T>
+{
+    private readonly ref T _reference;
+    private readonly int _length;
+
+    public int Length => _length;
+
+    public MyReadOnlySpan(T[] array)
+    {
+        _reference = ref array[0];
+        _length = array.Length;
+    }
+
+    public MyReadOnlySpan(ref T reference)
+    {
+        _reference = ref reference;
+        _length = 1;
+    }
+
+    public MyReadOnlySpan(ref T reference, int length)
+    {
+        _reference = ref reference;
+        _length = length;
+    }
+
+    public ref readonly T this[int index]
+    {
+        get
+        {
+            if (index >= _length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            return ref Unsafe.Add(ref _reference, index);
+        }
+
+    }
+
+    public MyReadOnlySpan<T> Slice(int offset)
+    {
+        if (offset > _length)
+        {
+            throw new IndexOutOfRangeException();
+        }
+
+        return new MyReadOnlySpan<T>(ref Unsafe.Add(ref _reference, offset), _length - offset);
+    }
 }
